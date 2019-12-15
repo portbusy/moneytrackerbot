f3057b3dc53908ca123443932d290e3444297d33
Davide <davides.bertolotti@gmail.com>
2019-12-16 00:26:44 +0100

    working add, remove, list outcome, list income functions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-moneytrakerBot
+# Description
+
+## Installtion
+* First create the `master.txt`  and `token.txt` files, filled respectively with your master chat id and telegram bot token 
---
+++ dbHelper.py
@@ -0,0 +1,52 @@
+import sqlite3
+
+
+class DBHelper:
+    def __init__(self, dbname="expenses.sqlite"):
+        self.dbname = dbname
+        self.conn = sqlite3.connect(dbname)
+
+    def setup(self):
+        outcome = "CREATE TABLE IF NOT EXISTS outcome (date date, value float, comment varchar(50))"
+        income = "CREATE TABLE IF NOT EXISTS income (date date, value float, comment varchar(50))"
+        self.conn.execute(outcome)
+        self.conn.execute(income)
+        self.conn.commit()
+
+    def add_income(self, date, value, comment):
+        stmt = "INSERT INTO income (date, value, comment) VALUES (?, ?, ?)"
+        args = (date, value, comment)
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def add_outcome(self, date, value, comment):
+        stmt = "INSERT INTO outcome (date, value, comment) VALUES (?, ?, ?)"
+        args = (date, value, comment)
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def delete_income(self, value, comment):
+        stmt = "DELETE FROM income WHERE value = (?) AND comment = (?)"
+        args = (value, comment)
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def delete_outcome(self, value, comment):
+        stmt = "DELETE FROM outcome WHERE value = (?) AND comment = (?)"
+        args = (value, comment)
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def get_income(self, month):
+        cur = self.conn.cursor()
+        stmt = "SELECT * FROM income WHERE strftime('%m', date) = '" +month+ "'"
+        cur.execute(stmt)
+        rows = cur.fetchall()
+        return rows
+
+    def get_outcome(self, month):
+        cur = self.conn.cursor()
+        stmt = "SELECT * FROM outcome WHERE strftime('%m', date) = '"+month+"'"
+        cur.execute(stmt)
+        rows = cur.fetchall()
+        return rows
